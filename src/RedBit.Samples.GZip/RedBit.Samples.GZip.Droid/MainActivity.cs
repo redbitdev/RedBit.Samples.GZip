6a008d7d23a60448d81b987e3b3cddeb8563888b
Mark Arteaga <mark@redbitdev.com>
2015-03-30 09:41:15 -0400

    initial commit

---
+++ src/RedBit.Samples.GZip/RedBit.Samples.GZip.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace RedBit.Samples.GZip.Droid
+{
+    [Activity(Label = "RedBit.Samples.GZip", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new RedBit.Samples.GZip.App());
+        }
+    }
+}
+
---
+++ src/RedBit.Samples.GZip/RedBit.Samples.GZip.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RedBit.Samples.GZip.WinPhone.Resources;
+
+namespace RedBit.Samples.GZip.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
