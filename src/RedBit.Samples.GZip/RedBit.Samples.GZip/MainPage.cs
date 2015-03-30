539d1bac587af1fe622d29dc2a7c270922b3cb23
Mark Arteaga <mark@redbitdev.com>
2015-03-30 10:33:33 -0400

    seperated out main page to it's own class

---
+++ src/RedBit.Samples.GZip/RedBit.Samples.GZip/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RedBit.Samples.GZip
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = 
+                {
+                    new Label 
+                    {
+                        XAlign = TextAlignment.Center,
+                        Text = "Welcome to Xamarin Forms!"
+                    }
+                }
+            };
+        }
+    }
+}
